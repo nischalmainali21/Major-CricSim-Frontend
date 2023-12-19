f71e992c78e97afab51d584a05b5fc748d4e70c2
nischalmainali21 <nischalmainali21@gmail.com>
2023-12-19 16:58:46 +0545

    search bar added

---
+++ src/components/Filter.js
@@ -0,0 +1,92 @@
+"use client"
+import React, { useState, useEffect } from 'react'
+import playerStatsData from '../../data/player_stats_data.json';
+
+const Filter = () => {
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredPlayers, setFilteredPlayers] = useState([]);
+  const [selectedPlayer, setSelectedPlayer] = useState(null);
+  
+  useEffect(() => {
+    // Filter players based on the search term
+    const filteredResults = Object.entries(playerStatsData)
+      .filter(([playerName, playerStats]) =>
+        playerName.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+      .map(([playerName, playerStats]) => ({ playerName }));
+
+    setFilteredPlayers(filteredResults);
+  }, [searchTerm]);
+
+  const handleNameClick = playerName => {
+    console.log(`Selected player: ${playerName}`);
+    setSelectedPlayer(playerName);
+    setSearchTerm('');
+  };
+  
+
+  return (
+    
+    <div>
+      {/* <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search by player name"
+          value={searchTerm}
+          onChange={e => setSearchTerm(e.target.value)}
+        />
+      </div> */}
+      <div className="flex p-2 rounded-lg relative">
+          
+          <input
+            className="border-b-2 p-4 "
+            onChange={e => setSearchTerm(e.target.value)}
+            type="text"
+            value={searchTerm}
+            placeholder="Search Players"
+          />
+          <div className="py-3 relative top-1 right-8 text-white font-semibold rounded-lg transition duration-3000 cursor-pointer">
+          <span><svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 opacity-30 text-dark hover:text-primary"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="black"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg></span>
+        </div>
+        </div>
+
+
+      {searchTerm.length > 0 && (
+        <ul>
+          {filteredPlayers.map(({ playerName }) => (
+            <li 
+            className='cursor-pointer w-[250px] hover:bg-blue-400'
+            key={playerName} 
+            onClick={() => handleNameClick(playerName)}
+            >
+              {playerName}
+            </li>
+          ))}
+        </ul>
+      )}
+
+  {selectedPlayer && (
+        <div>
+          <h2>Selected Player:</h2>
+          <p>{selectedPlayer}</p>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Filter
