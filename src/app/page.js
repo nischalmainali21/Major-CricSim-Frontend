ac33f3675fcb3a056215823704e1802cedcd5ce0
nischalmainali21 <nischalmainali21@gmail.com>
2023-12-19 14:17:40 +0545

    playerCard initial

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,19 @@
+import PlayerCard from "../components/playerCard";
+import playersData from "../../data/player_stats_data.json";
+
 export default function Home() {
+  const selectedPlayers = ["A Ashish Reddy", "A Badoni"];
+  const testPlayer = "A Ashish Reddy";
+  console.log(playersData[testPlayer]);
   return (
     <main>
-      <p>First Commit</p>
+      {selectedPlayers.map((playerName) => (
+        <PlayerCard
+          key={playerName}
+          playerStats={playersData[playerName]}
+          playerName={playerName}
+        />
+      ))}
     </main>
   );
 }
---
+++ src/components/playerCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PlayerCard = ({ playerStats, playerName }) => {
+  // console.log(player);
+  return (
+    <div>
+      <h1>{playerName}</h1>
+    </div>
+  );
+};
+
+export default PlayerCard;
