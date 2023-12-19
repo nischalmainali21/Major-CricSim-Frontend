fab195cc01a54f5d8faa2ce20bf537be0f038e4c
nischalmainali21 <nischalmainali21@gmail.com>
2023-12-19 22:59:05 +0545

    simulate initial

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,8 @@
-// import PlayerCard from "../components/PlayerCard";
-
 import { PlayerComparision } from "./playercomparision/page";
 
 export default function Home() {
-  // const selectedPlayers = ["A Ashish Reddy", "A Badoni"];
-
-  // const testPlayer = "A Ashish Reddy";
-  // console.log(playersData[testPlayer]);
   return (
     <main>
-      {/* <div className="flex flex-col gap-10">
-        {selectedPlayers.map((playerName) => (
-          <PlayerCard
-            key={playerName}
-            playerStats={playersData[playerName]}
-            playerName={playerName}
-          />
-        ))}
-      </div> */}
       <PlayerComparision />
     </main>
   );
