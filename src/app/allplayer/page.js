9ad9e11f9443a24232c1fa9680f859e1862f25f8
nischalmainali21 <nischalmainali21@gmail.com>
2024-02-13 09:33:58 +0545

    new table working

--- src/app/allplayer/page.js
+++ src/app/allplayer/page.js
@@ -1,11 +1,13 @@
 import React from "react";
 import playersData from "../../../data/player_stats_data.json";
 import AllPlayerTable from "@/components/AllPlayerTable";
+import NewAllPlayerTable from "@/components/NewAllPlayerTable";
 
 const AllPlayer = () => {
   return (
     <div className="p-4">
       <AllPlayerTable playersData={playersData} />
+      <NewAllPlayerTable playersData={playersData} />
     </div>
   );
 };
--- src/components/NewAllPlayerTable.jsx
+++ src/components/NewAllPlayerTable.jsx
@@ -1,4 +1,24 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+
+import {
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 const columns = [
   {
     accessorKey: "player_name",
@@ -79,8 +99,83 @@ const columns = [
   },
 ];
 
-const NewAllPlayerTable = () => {
-  return <div>NewAllPlayerTable</div>;
+const NewAllPlayerTable = ({ playersData }) => {
+  const [data, setData] = useState(() => []);
+
+  useEffect(() => {
+    const newList = [];
+    for (const [key, value] of Object.entries(playersData)) {
+      // Create a new object with the key as a property
+      const newObject = { player_name: key, ...value };
+      newList.push(newObject);
+    }
+
+    setData(newList);
+  }, []);
+
+  const table = useReactTable({
+    data,
+    columns,
+    // state: { columnFilters },
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    columnResizeMode: "onChange",
+  });
+
+  return (
+    <div>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                ))}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No Results
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
 };
 
 export default NewAllPlayerTable;
