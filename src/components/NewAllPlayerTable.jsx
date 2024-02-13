c138037a455db1f58b1183422371e0568b77e1b7
nischalmainali21 <nischalmainali21@gmail.com>
2024-02-13 09:12:24 +0545

    update new table initial

---
+++ src/components/NewAllPlayerTable.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+const columns = [
+  {
+    accessorKey: "player_name",
+    header: "Player",
+    cell: (props) => <p>{props?.getValue()}</p>,
+    enableColumnFilter: true,
+    filterFn: "includesString",
+  },
+  {
+    accessorKey: "batter_matches_played",
+    header: "Bat Matches",
+    cell: (props) => <p>{props?.getValue()}</p>,
+  },
+  {
+    accessorKey: "runs_scored",
+    header: "Runs Scored",
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+    accessorKey: "batting_average",
+    header: "Batting Avg",
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+    accessorKey: "strike_rate_x",
+    header: "Strike Rate",
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+    accessorKey: "explosivity_rating",
+    header: "Explosivity",
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+    accessorKey: "50_scored",
+    header: "Half Century",
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+    accessorKey: "100_scored",
+    header: "Century",
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+    accessorKey: "bowler_matches_played",
+    header: "Bowl Matches",
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+    accessorKey: "runs_conceded",
+    header: "Runs Conceded",
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+    accessorKey: "bowling_average",
+    header: "Bowling Avg",
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+    accessorKey: "strike_rate_y",
+    header: "Strike Rate",
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+    accessorKey: "economy",
+    header: "Economy",
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+    accessorKey: "wickets_taken",
+    header: "Wickets Taken",
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+  {
+    accessorKey: "balls_bowled",
+    header: "Balls Bowled",
+    cell: (props) => <p>{props.getValue()}</p>,
+  },
+];
+
+const NewAllPlayerTable = () => {
+  return <div>NewAllPlayerTable</div>;
+};
+
+export default NewAllPlayerTable;
