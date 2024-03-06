b295d496eac6b43c52d7dd1fa77e2338c38eedc6
nischalmainali21 <nischalmainali21@gmail.com>
2024-03-06 08:45:40 +0545

    Improve footer placement on each page Footer now places itself below the main content for each page.

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { ThemeProvider } from "@/components/theme-provider";
-import { ThemeToggle } from "@/components/ThemeToggle";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className}`}>
+      <body className={`${inter.className} min-h-screen flex flex-col `}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -23,8 +22,10 @@ export default function RootLayout({ children }) {
           disableTransitionOnChange
         >
           <Navbar />
-          {children}
-          <Footer />
+          <div className="flex-grow">{children}</div>
+          <div>
+            <Footer />
+          </div>
         </ThemeProvider>
       </body>
     </html>
