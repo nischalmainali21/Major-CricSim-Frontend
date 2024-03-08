f782549b335eeb0b27fc95288ad03d171b929bf1
nischalmainali21 <nischalmainali21@gmail.com>
2024-03-08 15:59:41 +0545

    Update state to store separate inning data

--- src/app/simulate/page.js
+++ src/app/simulate/page.js
@@ -3,24 +3,36 @@ import { Button } from "@/components/ui/button";
 import React from "react";
 import { getData } from "../../../actions/matchdata";
 import { useSimulateMatchData } from "../../../context/SimulateMatchDataContext";
+import SimulateFirstView from "@/components/SimulateFirstView";
 
 function Simulate() {
-  const { simulateMatchData, setSimulateMatchData } = useSimulateMatchData();
+  const {
+    simulateMatchData,
+    setSimulateMatchData,
+    simulateMatchFirstInningData,
+    setSimulateMatchFirstInningData,
+    simulateMatchSecondInningData,
+    setSimulateMatchSecondInningData,
+  } = useSimulateMatchData();
   async function handleClick() {
     try {
       const newData = await getData();
+      const { 1: firstData, 2: secondData } = newData[1312199];
       setSimulateMatchData(newData);
+      setSimulateMatchFirstInningData(firstData);
+      setSimulateMatchSecondInningData(secondData);
     } catch (error) {
       console.log("error fetchin data", error);
     }
   }
-  console.log(simulateMatchData);
+  // console.log(simulateMatchData);
+  // console.log(simulateMatchFirstInningData, simulateMatchSecondInningData);
   return (
     <div>
       <Button onClick={handleClick}>Simualte a Match</Button>
+      {simulateMatchData && <SimulateFirstView />}
     </div>
   );
 }
 
 export default Simulate;
-// onClick={handleClick()}
