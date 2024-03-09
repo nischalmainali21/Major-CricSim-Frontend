d5c347d6370bb95ed718a16469a7805394363137
nischalmainali21 <nischalmainali21@gmail.com>
2024-03-09 14:07:59 +0545

    Add function to fetch plot data

--- actions/matchdata.js
+++ actions/matchdata.js
@@ -11,3 +11,16 @@ export async function getData() {
 
   return res.json();
 }
+
+export async function getPlotData() {
+  const res = await fetch("http://localhost:8000/api/plot_data");
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
