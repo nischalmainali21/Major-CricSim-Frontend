26c47e14fa4c4a1e27fe43b949a960873fa3590c
nischalmainali21 <nischalmainali21@gmail.com>
2024-04-08 20:18:51 +0545

    Fix state variable name

--- src/app/allplayer/page.js
+++ src/app/allplayer/page.js
@@ -1,10 +1,12 @@
 import React from "react";
 import playersData from "../../../data/player_stats_data.json";
 import NewAllPlayerTable from "@/components/NewAllPlayerTable";
+import AllPlayerTeams from "@/components/AllPlayerTeams";
 
 const AllPlayer = () => {
   return (
     <div className="p-4">
+      <AllPlayerTeams />
       <NewAllPlayerTable playersData={playersData} />
     </div>
   );
