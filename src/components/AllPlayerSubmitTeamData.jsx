840f0e8487260ee892c6429435279774889299f3
nischalmainali21 <nischalmainali21@gmail.com>
2024-04-08 20:49:51 +0545

    Update team data sumbit button

--- src/components/AllPlayerSubmitTeamData.jsx
+++ src/components/AllPlayerSubmitTeamData.jsx
@@ -5,8 +5,35 @@ import { Button } from "./ui/button";
 
 const AllPlayerSubmitTeamData = () => {
   const { firstTeamData, secondTeamData } = useTeamData();
+  const disableButton =
+    firstTeamData.length === 11 && secondTeamData.length === 11 ? false : true;
 
-  return <Button>Simulate a Match</Button>;
+  const handleSubmitTeamData = async () => {
+    console.log(
+      "firstTeamData",
+      firstTeamData,
+      "secondTeamData",
+      secondTeamData
+    );
+    // const res = await fetch("http://localhost:8000/api/", {
+    //   method: "POST",
+    //   body: JSON.stringify({
+    //     firstTeamData,
+    //     secondTeamData,
+    //   }),
+    // });
+    // if (!res.ok) {
+    //   // This will activate the closest `error.js` Error Boundary
+    //   throw new Error("Failed to fetch data");
+    // }
+
+    // return res.json();
+  };
+  return (
+    <Button disabled={disableButton} onClick={handleSubmitTeamData}>
+      Simulate a Match
+    </Button>
+  );
 };
 
 export default AllPlayerSubmitTeamData;
