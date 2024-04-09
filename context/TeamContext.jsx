fb590dffb684707e07947ebda2398e4dda7002d7
nischalmainali21 <nischalmainali21@gmail.com>
2024-04-09 18:17:24 +0545

    Update state initial value in TeamContext

--- context/TeamContext.jsx
+++ context/TeamContext.jsx
@@ -4,8 +4,32 @@ import React, { createContext, useContext, useState } from "react";
 const TeamDataContext = createContext(null);
 
 export const TeamDataContextProvider = ({ children }) => {
-  const [firstTeamData, setFirstTeamData] = useState([]);
-  const [secondTeamData, setSecondTeamData] = useState([]);
+  const [firstTeamData, setFirstTeamData] = useState([
+    "A Symonds",
+    "AC Gilchrist",
+    "SR Watson",
+    "AD Russell",
+    "BA Stokes",
+    "CH Gayle",
+    "CH Morris",
+    "JC Archer",
+    "KA Pollard",
+    "KP Pietersen",
+    "Shakib Al Hasan",
+  ]);
+  const [secondTeamData, setSecondTeamData] = useState([
+    "SR Tendulkar",
+    "Z Khan",
+    "RG Sharma",
+    "V Kohli",
+    "MS Dhoni",
+    "KH Pandya",
+    "HH Pandya",
+    "JJ Bumrah",
+    "SK Raina",
+    "YK Pathan",
+    "YS Chahal",
+  ]);
   return (
     <TeamDataContext.Provider
       value={{
